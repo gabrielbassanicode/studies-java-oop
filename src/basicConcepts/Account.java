8e5b633e562e446070a800a6d91b8ef719eef634
gabrielbassanicode <gabrielbassani.code@gmail.com>
2026-08-17 14:10:05 -0300

    Init

---
+++ src/basicConcepts/Account.java
@@ -0,0 +1,57 @@
+package basicConcepts;
+
+public class Account {
+
+    private final int accountNumber;
+    private String name;
+    private double balance;
+
+    public Account(int accountNumber, String name, double initial) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        deposit(initial);
+
+    }
+
+    public Account(int accountNumber, String name) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double value) {
+        balance += value;
+
+    }
+
+    public void withdraw(double value) {
+        balance -= value + 5.0;
+
+    }
+
+    public String toString() {
+        return "Account " +
+                accountNumber
+                + ", Holder: "
+                + name
+                + ", Balance: $"
+                + balance;
+
+    }
+}
---
+++ src/basicConcepts/Main.java
@@ -0,0 +1,54 @@
+package basicConcepts;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Account account;
+
+
+        System.out.print("Enter account number: ");
+        int accountNumber = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter account holder: ");
+        String name = sc.nextLine();
+
+        System.out.print("Is there na initial deposit? (y/n): ");
+        String choice = sc.nextLine();
+
+        if (choice.equals("y")) {
+           System.out.print("Enter the initial deposit: ");
+           double initial = sc.nextDouble();
+           account = new Account(accountNumber, name, initial);
+
+        }else {
+            account = new Account(accountNumber, name);
+
+        }
+
+        System.out.println("Account data: " + account);
+
+        System.out.print("Enter a value to deposit: ");
+        double value = sc.nextDouble();
+        account.deposit(value);
+
+
+        System.out.println("Updated account data: " + account);
+
+        System.out.print("Enter a value to withdrawal($5.00 Tax): ");
+        value = sc.nextDouble();
+
+        account.withdraw(value);
+
+        System.out.println("Update account data: " + account);
+
+        sc.close();
+    }
+
+}
